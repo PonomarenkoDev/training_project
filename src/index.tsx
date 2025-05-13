8d07743f1c335e7e6818e4d10cc7be4b5152ec14
PonomarenkoDev <ponomarenkoanna0904@gmail.com>
2025-05-13 15:10:09 +0600

    update files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
-import App from "./App";
-import ThemeProvider from "./theme/ThemeProvider";
+import App from "./app/App";
+import {ThemeProvider} from "app/providers/ThemeProvider";
+
+import "./shared/config/i18n/i18n"
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
