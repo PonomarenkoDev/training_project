5f7ca70d53559b609bd28a92667236d7e35f02d7
PonomarenkoDev <ponomarenkoanna0904@gmail.com>
2025-05-14 12:20:34 +0600

    add eslint file

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import parser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginImport from 'eslint-plugin-import';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.{js,ts,jsx,tsx}'],
+        languageOptions: {
+            parser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+                __IS_DEV__: true,
+                require: true
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            react: pluginReact,
+            import: pluginImport,
+        },
+        rules: {
+            'react/jsx-indent': [2, 4],
+            'react/jsx-indent-props': [2, 4],
+            indent: [2, 4],
+            'react/jsx-filename-extension': [2, {extensions: ['.js', '.jsx', '.tsx']}],
+            'import/no-unresolved': 'off',
+            'import/prefer-default-export': 'off',
+            'no-unused-vars': 'warn',
+            'react/require-default-props': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'react/jsx-props-no-spreading': 'warn',
+            'react/function-component-definition': 'off',
+            'no-shadow': 'off',
+            'import/extensions': 'off',
+            'import/no-extraneous-dependencies': 'off',
+            'no-underscore-dangle': 'off',
+        },
+    },
+];
